4059ac10ad26694a3fce995c2b969d9e7425990d
Monica Bustamante <58006376+Moly-malibu@users.noreply.github.com>
2024-10-18 15:14:01 -0500

    Update

--- app.py
+++ app.py
@@ -5,16 +5,25 @@ import datetime
 import requests
 from requests import get
 import pandas as pd
+import numpy as np
+#import category_encoders as ce
+#from sklearn.impute import SimpleImputer
+#from sklearn.ensemble import RandomForestClassifier
+#from sklearn.model_selection import train_test_split
+#from sklearn.pipeline import make_pipeline
+ 
 
-st.markdown("<h1 style='text-align: center; color: #002967;'>Software Engineer</h1>", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: #002967;'>App String Palindrome</h1>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: #002967;'>Business Analysis</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #002967;'>Agriculture Crop Production by Countries: Vegetales & Fruits</h1>", unsafe_allow_html=True)
 def main():
     # Register pages
     pages = {
         "Home": Home,
-        "Palindrome": Palindrome,
+        "datainfo": datainfo,
+        "Permutation": Permutation,
     }
-    st.sidebar.title("Palindrome App")
+    st.sidebar.title("Statistics")
     page = st.sidebar.selectbox("Select Menu", tuple(pages.keys()))
     pages[page]()
 
@@ -34,18 +43,8 @@ def Home():
         Artificial Intelligence helps you to perform a successful future, taking the correct decisions by little effort! 
         ---
 
-        App:
-
-        1. Check if the input is a palindrome (reads the same both forwards and backwards.
-                
-        2. If the input length is odd, find and return the letter in the middle of the string.
-                
-        3. Send appropriate responses to the frontend:
-                Example Input	: 
-                        "racecar" Example Output: "The input 'racecar' is a palindrome and its middle letter is 'e'."
-                Example Input:	 
-                        "hello" Example Output: "The input 'hello' is not a palindrome."
-        ---
+        The aim is to analyze the performance of the Agro-Business industry from 1961 to 2014. Historical data reveals that while the banana industry once faced significant challenges, it has defied initial predictions. Despite forecasts suggesting a bleak future, scientific advancements have played a crucial role in ensuring bananas remain a staple in the global food basket. Although the banana industry has experienced slower growth compared to other sectors within Agribusiness, its resilience over the past six decades is a testament to the power of innovation and adaptation.
+                        ---
         """)
         today = st.date_input('Today is', datetime.datetime.now())
 
@@ -63,7 +62,7 @@ def Home():
             <div class="col l3 s12">
                     <h5 class="white-text">Connect With Me</h5>
                     <ul>
-                        <a href="http://www.monicadatascience.com/" target="#002967" class="white-text">
+                        <a href="/" target="#002967" class="white-text">
                         ❤<i class="❤"></i>
                     </a>
                     <a href="https://www.linkedin.com/in/monica-bustamante-b2ba3781/3" target="#002966" class="white-text">
@@ -73,7 +72,7 @@ def Home():
                     </a>
                     <a href="https://github.com/Moly-malibu/UNHCR.git" target="#002967" class="white-text">
                         <i class="fab fa-github-square fa-4x"></i>
-                    <a href="http://www.monicadatascience.com/" target="#002967" class="white-text">
+                    <a href="/" target="#002967" class="white-text">
                     ❤<i class="❤"></i>
                     </a>
                     </ul>
@@ -82,7 +81,7 @@ def Home():
                 </div>
                 <div class="footer-copyright">
                 <div class="container">
-                <a class="white-text text-lighten-3" href="http://www.monicadatascience.com/">Made by Monica Bustamante</a><br/>
+                <a class="white-text text-lighten-3" href="/">Made by Liliana Bustamante</a><br/>
                 <a class="white-text text" href=""> @Copyrigh</a>
                 </div>
                 </div>
@@ -110,7 +109,7 @@ title_temp = """
 	"""
 components.html(title_temp,height=100)
     
-def Palindrome():
+def datainfo():
     page_bg_img = '''
     <style>
     .stApp {
@@ -120,36 +119,99 @@ def Palindrome():
     </style>
     '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; color: #002966;'>Example</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: #002966;'>Agricultural Production</h1>", unsafe_allow_html=True)
     
-    palinwords = 'https://raw.githubusercontent.com/Moly-malibu/UNHCR/main/wordspalid.csv'
-    df = pd.read_csv(palinwords)
-    words = st.sidebar.selectbox('Palindrome', (df))
-    st.markdown("<h1 style='text-align: center; color: #002967;'>String</h1>", unsafe_allow_html=True)
-    word = st.sidebar.selectbox('Done', (df))
+    df = pd.read_csv('https://raw.githubusercontent.com/Moly-malibu/agriculture-crop-production/master/agriculture-crop-production%20(11).csv').drop(['Unnamed: 0'], axis=1)
+    df.replace(np.nan, 0, inplace=True)
+    info = st.sidebar.selectbox('Data Set Info', (df))
+    st.markdown("<h1 style='text-align: center; color: #002967;'>By Countries</h1>", unsafe_allow_html=True)
+    info = st.sidebar.selectbox('Done', (df))
+    shape=(df.shape)
     
-    if(word == word[: : -1]):
-        st.write("Is Palindrome")
-    else:
-        st.write("Is Not a Palindrome")
+    st.write("Production by Countries:", df)
+    st.write("Dimension DataSet:", shape, "Shape")  
+    info= df.describe()
+    st.write("Statistics Report DataSet:", info)
     
-    def is_palindrome(word): 
-        if len(word) == 0: 
-            return True
-        left = 0
-        right = len(word)-1
-        while (left < right): 
-            if word[left] != word[right]: 
-                return False
-            left += 1; right -= 1
-            return True
-        return is_palindrome
+    items = df.groupby(['Item', 'Element', 'Unit']).size().reset_index(name='Products')
+    st.write("Products:", items)
     
-    st.write('***Word or String***', word)
-    if len(word) % 2 == 0:
-        st.write(word[len(word) // 2 - 1:len(word) // 2 + 1])
-    else:
-        st.write('middle letter is',word[len(word) // 2])
+    items= df['Item'].describe()
+    st.write("Statistic Describe: Targe Products:", items)
+
+    y = df['Item']
+    y.nunique()
+    items = y.value_counts(normalize=True)
+    st.write("Binary Classification: if the classes are imbalanced:", items)
+
+    items= df['Item'].unique()
+    st.write("classification of agricultural products:", items)
+
+    items = df['Item'].value_counts().nlargest(100)
+    st.write("Main agricultural products cultivated:", items)
+
+    df['Item'] = df['Item'].str.lower()
+    blueberries = df['Item'].str.contains('blueberries')
+    gums = df['Item'].str.contains('gums')
+    peppermint = df['Item'].str.contains('peppermint')
+    tallowtree = df['Item'].str.contains('tallowtree')
+    roots = df['Item'].str.contains('roots')
+    vegetablesmelons = df['Item'].str.contains('vegetablesmelons')
+    vegetables = df['Item'].str.contains('vegetables')
+    kola = df['Item'].str.contains('kola')
+    nuts = df['Item'].str.contains('nuts')
+    melons = df['Item'].str.contains('melons')
+    cereals = df['Item'].str.contains('cereals')
+    bananas = df['Item'].str.contains('bananas')
+
+    df.loc[vegetables, 'Item'] = 'vegetables'
+    df.loc[roots, 'Item'] = 'roots'
+    df.loc[kola, 'Item'] = 'kola'
+    df.loc[nuts, 'Item'] = 'nuts'
+    df.loc[melons, 'Item'] = 'melons' 
+    df.loc[cereals, 'Item'] = 'cereals'
+    df.loc[bananas, 'Item'] = 'bananas'
+
+    df.loc[~blueberries & ~gums & ~roots & ~kola & ~nuts & ~melons & ~cereals &
+       ~peppermint & ~tallowtree & ~vegetables & ~vegetablesmelons & ~bananas, 'Item'] = 'Other'
+    items1 = df['Item'].value_counts()
+    st.write("Subclassification:", items1)
+
+def Permutation():
+    test = pd.read_csv('https://raw.githubusercontent.com/Moly-malibu/agriculture-crop-production/master/agriculture-crop-production%20(11).csv').drop(['Unnamed: 0'], axis=1) 
+    val = pd.read_csv('https://raw.githubusercontent.com/Moly-malibu/agriculture-crop-production/master/agriculture-crop-production%20(11).csv').drop(['Unnamed: 0'], axis=1)  
+    train = pd.read_csv('https://raw.githubusercontent.com/Moly-malibu/agriculture-crop-production/master/agriculture-crop-production%20(11).csv').drop(['Unnamed: 0'], axis=1)
+    train['Item'].mode()   
+
+    test.replace(np.nan, 0, inplace=True)
+    val.replace(np.nan, 0, inplace=True)
+    train.replace(np.nan, 0, inplace=True)
+    train.describe(exclude='number')
+
+    val['Item'].value_counts()
+    bananas = val[val.Item=='Bananas']
+    st.write("Production:", bananas)
+
+    target = 'Element'
+    train[target].value_counts(normalize=True)
+    features = train.select_dtypes('number').columns.drop(target)
+    X_train = train[features]
+    y_train = train[target]
+    X_val = val[features]
+    y_val = val[target]
+    X_test = test[features]
+    y_test = test[target]
+
+    pipeline = make_pipeline(
+        ce.OrdinalEncoder(), 
+        SimpleImputer(strategy='median'), 
+        RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
+    )
+
+    # Fit on train, score on val
+    pipeline.fit(X_train, y_train)
+    st.write('Validation Accuracy', pipeline.score(X_val, y_val))
+
 
 
 if __name__ == "__main__":
